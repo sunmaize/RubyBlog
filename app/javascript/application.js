0200bb83e728eabacf340435c0359e9e71f9469f
sunmaize <770574229@qq.com>
2024-09-27 16:01:12 -0700

    Add delete article function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "@rails/ujs"
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+Rails.start()
