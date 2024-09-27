57edce0de84f97e42485280fd957e9039490780a
sunmaize <770574229@qq.com>
2024-09-27 16:28:47 -0700

    Add comment model& function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "welcome/index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
